23b466bde1678884cd0d4326f608af57018bfe52
erwanfanya <fanyaerwan@gmail.com>
2026-08-19 18:28:23 -0400

    Refactor calculator functions and add operator input
    Refactored calculator functions and added operator input handling.

--- calculator.py
+++ calculator.py
@@ -4,17 +4,25 @@ def add(x,y):
 def subtract(x,y):
     return x-y
 
-def multiply(x,y)
+def multiply(x,y):
     return x*y
 
 def divide(x,y):
     return x/y
 
-num1 = int(input("enter Number 1:"))
+operator = input("enter one of the operator (+,-,*,/):")
+
+num1 = int(input("enter number 1:"))
 num2 = int(input("enter number 2:"))
 
-print("sum :",add(num1,num2))
-print("difference:", subtract(num1,num2))
-print("product :", multiply(num1,num2))
-print("quotion:", divide(num1,num2))
 
+if operator == "+":
+    print("sum :", (num1+num2))
+elif operator == "-":
+    print("difference", (num1-num2))
+elif operator == "*":
+    print("product", (num1,num2))
+elif operator == "/":
+    print("quotion", (num1/num2))
+else:
+    print("invalid operator")
